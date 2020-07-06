a6a4d8dfeea33b6a134a81fecbd6d6f84937b2e0
ccadori <carloscadori.dev@gmail.com>
2020-07-05 23:05:30 -0300

    adding unit tests

---
+++ Tests/MachineTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using Sand.StateMachine;
+using System;
+
+namespace Tests
+{
+    public class TestState : State
+    {
+        public int runCounter;
+
+        public TestState(Action<Machine> exitAction) : base(exitAction)
+        {
+
+        }
+
+        public override void Run()
+        {
+            base.Run();
+
+            runCounter ++;
+        }
+    }
+
+    public class MachineTests
+    {
+        [Test]
+        public void ShouldStartRootState()
+        {
+            var machine = new Machine(null);
+            var state = new State(null);
+            machine.Add("awd", state, true);
+
+            Assert.IsNull(machine.CurrentState);
+            
+            machine.Start();
+
+            Assert.AreEqual(machine.CurrentState, state);
+        }
+
+        [Test]
+        public void ShouldSetTheRootState()
+        {
+            var machine = new Machine(null);
+            var state = new State(null);
+            machine.Add("awd", state, true);
+
+            Assert.AreEqual(machine.RootState, state);
+        }
+
+        [Test]
+        public void ShouldExecuteRunOnCurrentState()
+        {
+            var machine = new Machine(null);
+            var state = new TestState(null);
+            machine.Add("awd", state, true);
+            machine.Start();
+
+            Assert.AreEqual(state.runCounter, 0);
+
+            machine.Run();
+
+            Assert.AreEqual(state.runCounter, 1);
+        }
+    
+        [Test]
+        public void ShouldChangeToTheRightState()
+        {
+            var machine = new Machine(null);
+            var idle = new State(null);
+            var running = new State(null);
+            
+            machine.Add("idle", idle, true);
+            machine.Add("running", running, false);
+
+            machine.Start();
+
+            Assert.AreEqual(machine.CurrentState, idle);
+
+            machine.ChangeState("running");
+
+            Assert.AreEqual(machine.CurrentState, running);
+        }
+    }
+}
---
+++ Tests/StateTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Sand.StateMachine;
+
+namespace Tests
+{
+    public class StateTests
+    {
+        [Test]
+        public void ShouldCallExitAction()
+        {
+            bool exitWasCalled = false;
+            var state = new State((Machine machine) => exitWasCalled = true);
+            state.Run();
+
+            Assert.IsTrue(exitWasCalled);
+        }
+    }
+}
